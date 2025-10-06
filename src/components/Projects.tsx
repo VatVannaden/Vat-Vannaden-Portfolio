7291a2fffc178a46c8c819410785ceafac089b48
Vat Vannaden <vat.vannaden.2822@rupp.edu.kh>
2025-10-06 19:23:37 +0700

    chore: Remove unused useEffect import in Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { FiGithub, FiLinkedin, FiMail, FiExternalLink, FiArrowLeft, FiArrowRight } from 'react-icons/fi';
 import { motion } from 'framer-motion';
 import ParticlesBg from './ParticlesBg'
